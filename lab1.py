918928095b2377ccdb84db3efd3f580a263d573f
danila seliayeu <dan.seliayeu@gmail.com>
2021-09-02 14:41:37 -0600

    update script to fetch and print itself

--- lab1.py
+++ lab1.py
@@ -2,3 +2,4 @@ import requests
 print(requests.__version__)
 
 requests.get("http://www.google.com/")
+print(requests.get("https://raw.githubusercontent.com/seliayeu/cmput404-lab1/main/lab1.py").text)
